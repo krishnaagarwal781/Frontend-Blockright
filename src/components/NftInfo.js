aff2ffbf1766f4a7ce5f7cb0aa4501cde1436065
Krishna <krishnacool781@gmail.com>
2023-09-13 23:39:37 +0530

    Changes to be committed: 	modified:   src/components/NftInfo.js

--- src/components/NftInfo.js
+++ src/components/NftInfo.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
-import axios from "axios"; // For making HTTP requests
+import React, {useState, useContext } from "react";
 import WalletIDMainContext from "../context/walletID/WalletIDMainContext";
 
 function NFTInfo() {
